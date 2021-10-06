0c6bc8c03d28111e44ec2afd528778d5d82b5450
sloanliam <sloanliam1091@gmail.com>
2021-10-06 11:27:33 -0400

    initial commit 2

---
+++ cicdpractice/Calculator.cs
@@ -0,0 +1,13 @@
+namespace cicdpractice
+{
+    public class Calculator
+    {
+        public int Add(int a, int b){
+            return a + b;
+        }
+
+        public int Multiply(int a, int b){
+            return a * b;
+        }
+    }
+}
---
+++ cicdpractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cicdpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine(calculator.Add(2, 2));
+            Console.WriteLine(calculator.Multiply(2, 2));
+        }
+    }
+}
