e1e2116afa8b1f01d29160e1bda37e4d27af332c
sloanliam <85837717+sloanliam@users.noreply.github.com>
2021-10-06 12:03:35 -0400

    Update UnitTest1.cs

--- testing/UnitTest1.cs
+++ testing/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace testing
         public void AddTest()
         {
             Calculator c = new Calculator();
-            int expected = 5;
+            int expected = 4;
             Assert.Equal(c.Add(2, 2), expected);
         }
 
